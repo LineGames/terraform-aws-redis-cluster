40e0805b5be8ff03e44667b34e385e26208690c9
kyeongju <ainimo2@gmail.com>
2022-12-14 14:29:10 +0900

    redis

---
+++ redis.tf
@@ -0,0 +1,27 @@
+
+resource "aws_elasticache_replication_group" "this" {
+  count                      = var.node_groups != null ? 1 : 0
+  replication_group_id       = var.name
+  description                = var.description
+  subnet_group_name          = var.subnet_group_name
+  security_group_ids         = var.security_groups
+  engine                     = var.engine
+  engine_version             = var.engine_version
+  node_type                  = var.instance_type
+  port                       = var.port
+  parameter_group_name       = var.parameter_group_name
+  automatic_failover_enabled = var.automatic_failover_enabled
+  num_node_groups            = var.node_groups
+  replicas_per_node_group    = var.replicas
+  tags                       = var.tags
+}
+
+
+
+
+
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type    = string
+  default = ""
+}
+
+variable "parameter_group_name" {
+  type = string
+}
+
+variable "subnet_group_name" {
+  type    = string
+  default = null
+}
+
+variable "subnets" {
+  type    = list(string)
+  default = []
+}
+
+variable "security_groups" {
+  type    = list(string)
+  default = []
+}
+
+variable "node_groups" {
+  description = "Number of node groups (shards) for this Redis replication group."
+  type        = number
+  default     = null
+}
+
+variable "replicas" {
+  description = "Number of replica nodes in each node group."
+  type        = number
+  default     = null
+}
+
+variable "engine" {
+  type = string
+}
+
+variable "engine_version" {
+  type = string
+}
+
+variable "port" {
+  type = number
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "automatic_failover_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+
