31b756af7bc8fed832deabe365bf4daf2c3eca35
kyeongju <ainimo2@gmail.com>
2022-12-15 15:49:26 +0900

    Update redis.tf

--- redis.tf
+++ redis.tf
@@ -1,6 +1,6 @@
 
 resource "aws_elasticache_replication_group" "this" {
-  count                      = var.node_groups != null ? 1 : 0
+  count                      = var.replication_group_id != null ? 1 : 0
   replication_group_id       = var.replication_group_id
   description                = var.description
   subnet_group_name          = var.subnet_group_name
@@ -11,9 +11,11 @@ resource "aws_elasticache_replication_group" "this" {
   port                       = var.port
   parameter_group_name       = var.parameter_group_name
   automatic_failover_enabled = var.automatic_failover_enabled
-  num_node_groups            = var.node_groups
+  num_cache_clusters         = var.num_cache_clusters
   replicas_per_node_group    = var.replicas_per_node_group
+  multi_az_enabled           = var.multi_az_enabled
   tags                       = var.tags
+  
 }
 
 
