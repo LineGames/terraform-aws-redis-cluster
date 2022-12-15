d603e3bda66804e2162fe9f9cddbff44930d434c
kyeongju <ainimo2@gmail.com>
2022-12-15 15:52:49 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,35 +7,14 @@ variable "description" {
   default = ""
 }
 
-variable "parameter_group_name" {
-  type = string
-}
-
 variable "subnet_group_name" {
   type    = string
   default = null
 }
 
-variable "subnets" {
-  type    = list(string)
-  default = []
-}
-
 variable "security_group_ids" {
   type    = list(string)
   default = []
-}
-
-variable "node_groups" {
-  description = "Number of node groups (shards) for this Redis replication group."
-  type        = number
-  default     = null
-}
-
-variable "replicas_per_node_group" {
-  description = "Number of replica nodes in each node group."
-  type        = number
-  default     = null
 }
 
 variable "engine" {
@@ -46,15 +25,37 @@ variable "engine_version" {
   type = string
 }
 
-variable "port" {
-  type = number
-}
-
 variable "instance_type" {
   type = string
 }
 
+variable "port" {
+  type = number
+}
+
+variable "parameter_group_name" {
+  type = string
+}
+
 variable "automatic_failover_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "num_cache_clusters" {
+  description = "Number of node groups (shards) for this Redis replication group."
+  type        = number
+  default     = null
+}
+
+
+variable "replicas_per_node_group" {
+  description = "Number of replica nodes in each node group."
+  type        = number
+  default     = null
+}
+
+variable "multi_az_enabled" {
   type    = bool
   default = true
 }
